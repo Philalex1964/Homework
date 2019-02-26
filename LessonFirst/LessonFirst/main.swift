3e757155ede7bc37b86ce588d9bb8b6f41cb73b0
Alexander <Philalex196401@gmail.com>
2019-02-27 02:05:15 +0300

    конец урока

--- LessonFirst/LessonFirst/main.swift
+++ LessonFirst/LessonFirst/main.swift
@@ -1,15 +1,23 @@
-
-
 import Foundation
 
-var valueFirst : Int = 15
-var valueSecond : Int? = 5
-valueSecond = nil
+func test () {
+    print(1)
+    testSecond ()
+}
+
+func testSecond () {
+    print(2)
+}
 
 
+var array = [1,2,3,4,5,1,2,3,4,5]
+array.append(4)
+array.append(5)
+print(array)
+var value : Set<Int> = [1,2,3,4,5,1,2,3,4,5]
+var valueSecond : Set<Int> = [1,4,25,101]
+print(value.subtracting(valueSecond))
+test()
+print(123)
 
-
-
-
-print(valueSecond)
 
