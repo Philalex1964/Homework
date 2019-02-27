873cf29e2f4f5d88e2cf9755c4a33edeedba1afc
Alexander <Philalex196401@gmail.com>
2019-02-27 20:52:01 +0300

    Homework Lesson1 - 2

---
+++ 1l_FilippovAlexander/1l_FilippovAlexander/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1l_FilippovAlexander
+//
+//  Created by Александр Филиппов on 27.02.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
