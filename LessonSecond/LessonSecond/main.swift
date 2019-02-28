2cbe9faf669dba91c8b0df17049e132cb235b680
Alexander <Philalex196401@gmail.com>
2019-02-28 15:23:08 +0300

    LessonSecond

---
+++ LessonSecond/LessonSecond/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LessonSecond
+//
+//  Created by Александр Филиппов on 28.02.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
