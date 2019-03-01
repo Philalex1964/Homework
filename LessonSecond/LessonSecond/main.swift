0173c7bd31dec2240cb902f4a84b8db51efdd9d2
Alexander <Philalex196401@gmail.com>
2019-03-01 22:42:30 +0300

    switch case

--- LessonSecond/LessonSecond/main.swift
+++ LessonSecond/LessonSecond/main.swift
@@ -9,16 +9,33 @@
 import Foundation
 
 
-
-
-let x = 10
-if x == 20 {           // условие неверно, код в блоке не выполнится
-    print("perform")
-} else if x == 30 {   // условие неверно, код в блоке не выполнится
-    print("perform again")
-} else {
-    print("default")
+let point = (1, 2)
+switch point {
+case (0, 0):
+    print("(0, 0) является точкой отсчета.")
+case (-2...2, -2...2):
+    print("(\(point.0), \(point.1)) находится рядом с точкой отсчета.")
+default:
+    print("Точка имеет координаты (\(point.0), \(point.1)).")
 }
+// Выведет "(1, 2)находится рядом с точкой отсчета."
+
+
+
+
+let a = 7
+switch  a   {
+case 1:
+    print ("case 1")
+case 7, 8:
+    print ("Case 2")
+case 3:
+    print ("Case 3")
+default:
+    print ("Default")
+    // выполняется, если ни один из вариантов не будет соответствовать значению
+}
+
 
 
 
