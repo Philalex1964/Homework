4f1f22264f86bcef338750731b8bf30109c9dc5d
Alexander <Philalex196401@gmail.com>
2019-03-02 18:34:03 +0300

    Functions

--- LessonSecond/LessonSecond/main.swift
+++ LessonSecond/LessonSecond/main.swift
@@ -8,26 +8,47 @@
 
 import Foundation
 
-let array = [5,12,7,32]
-for element in array {
-    let z = element * 2
-    print (z)
+// Функции
+sqrt(4)               // мы передали функции параметр «4», она вернула новое значение «2», которое мы никак не использовали
+let x = sqrt(4)      // здесь мы присваиваем возвращаемое функцией значение переменной «x»
+let y = 7 + sqrt(4) // используем результат функции в выражении
+
+
+/*func имя_функции ( внешнее_имя_параметра внутреннее_имя_параметра: тип_параметра ) -> тип_возвращаемого_значения
+func имя_функции ( внешнее_и_внутреннее_имя_параметра: тип_параметра ) -> тип_возвращаемого_значения
+func имя_функции ( _ внутреннее_имя_параметра: тип_параметра ) -> тип_возвращаемого_значения*/
+
+/*func formatArray (_ array : [String], good : String, andRemoveEvil evil : String) -> [String] {
+    var newArray = array
+    newArray.append(good)
+    if let index = newArray.index(of: evil) {
+    newArray.remove(at: index)
+}
+return newArray
 }
 
-for _ in 1...5 {
-    print ("repeat")
+let array = ["яблоко", "персик", "лук"]
+
+let goodArray = formatArray(array, good: "клубника", andRemoveEvil: "лук")
+print(goodArray)*/
+
+func formatArray (_ array : [String], good : String, evil : String?) -> [String] { //убрали внешнее имя третьего параметра и сделали его опциональным
+    var newArray = array
+    newArray.append(good)
+    if let evil = evil {
+        if let index = newArray.index(of: evil) {
+        newArray.remove(at: index)
+    }
+}
+    return newArray
 }
 
-for i in stride(from: 0, to: 5, by: 2) {
-    print (i)
-}
-print("=====")
+let array = ["яблоко", "персик", "лук"]
 
-for i in stride(from: 0, to: 9, by: 3) {
-    print (i)
-}
+let goodArrayWithEvil = formatArray(array, good: "клубника", evil: nil)
 
-for element in array where element % 2 == 0 {
-    print (element)
-}
+print(goodArrayWithEvil)
 
+let goodArrayWithoutEvil = formatArray(array, good: "банан", evil: "лук")
+
+print(goodArrayWithoutEvil)
