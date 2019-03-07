7481a21a484189cfb6172fd45106b2e640341c01
Alexander <Philalex196401@gmail.com>
2019-03-07 22:41:14 +0300

    lessson4

--- LessonFourth/LessonFourth/main.swift
+++ LessonFourth/LessonFourth/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+
+enum HondaDoorState {
+    case open, close
+}
+enum Transmission {
+    case manual, auto
+}
+
+enum Color {
+    case white, black
+}
+
+struct Honda {
+    let color: Color
+    let mp3: Bool
+    let transmission: Transmission
+    var km: Double
+    var doorState: HondaDoorState
+}
+var car1 = Honda(color: .white, mp3: true, transmission: .auto, km: 0.0, doorState: .close)
+var car2 = Honda(color: .white, mp3: false, transmission: .auto, km: 120.0, doorState: .close)
+print(car1.km,", ", car2.km)   //0.0, 120.0
+car2 = car1
+print(car1.km,", ", car2.km)  //0.0, 0.0
+car1.km = 500.0
+print(car1.km,", ", car2.km) //500.0, 0.0
+
+
+
+
+
+
+
 
