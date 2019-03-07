b98ab04defbaad138322d547213e8ae81309e43c
Alexander <Philalex196401@gmail.com>
2019-03-07 16:27:59 +0300

    init

--- 3l_FilippovAlexander/3l_FilippovAlexander/main.swift
+++ 3l_FilippovAlexander/3l_FilippovAlexander/main.swift
@@ -41,25 +41,68 @@ enum TrunkState {
 }
 
 struct Vehicle {
-    var vehicleType = VehicleType
+    var vehicleType : VehicleType
     var mark : Mark
     var productionYear : Int
-    var trunkVolume : Int = 0
-    var engineState = EngineState
+    var trunkVolume : Int = 0 { // Контроль оставшегося места в автомобиле
+        willSet {
+            if (trunkState == .empty) || (trunkVolume > 0) && (trunkVolume != 0) && (newValue < trunkVolume) {
+                let space = trunkVolume - newValue
+                print ("Free space in the Trunk: \(space)")
+            } else { print("Trunk is full.")}
+        }
+    }
+    var engineState : EngineState
     var windowsState : WindowsState
-    var trunkState = TrunkState
+    var trunkState : TrunkState
+    var model : String
+    
+    
+    
+    // Контроль выключения двигателя. Специально даю задачу привести двигатель в неработающее состояние
+    mutating func stopEngine() {
+        engineState = engineState == .on ? .off: .off
+            print ("Engine is off")
+    }
+    
+    // Контроль закрытия окон. То же самое
+    mutating func shutWindow() {
+        windowsState = windowsState == .open ? .close : .close
+        print ("Windows are closed")
+    }
+    
+    // Контроль разгрузки/загрузки
+    mutating func loadUnload() {
+        trunkState = trunkState == .empty ? .full : .empty
+        if trunkState == .empty {
+            print ("Ready for Loading")
+        } else{
+        print ("Ready for Unloading")
+        }
+    }
+    init (vehicleType: VehicleType, mark: Mark, productionYear: Int, trunkVolume: Int) {
+        model = "AMG 63"
+        self.mark = mark
+        self.vehicleType = vehicleType
+        self.productionYear = productionYear
+        self.trunkVolume = trunkVolume
+    }
 }
-var car1 = Vehicle(vehicleType: .car, mark: .mercedes, productionYear: 2018, trunkVolume: 0, engineState: .off, windowsState: .close, trunkState: .empty)
-
-var truck1 = Vehicle(vehicleType: .car, mark: .mercedes, productionYear: 2018, trunkVolume: 0, engineState: .off, windowsState: .close, trunkState: .empty)
-
-var  car2 = Vehicle(vehicleType: <#T##VehicleType.Type#>, mark: <#T##Mark#>, productionYear: <#T##Int#>, trunkVolume: <#T##Int#>, engineState: <#T##EngineState.Type#>, windowsState: <#T##WindowsState#>, trunkState: <#T##TrunkState.Type#><#value#>
+// здесь требует декларацию ((( не понимаю, что надо сделать}
+    
+    
 
 
+
+//var car1 = Vehicle(vehicleType: .car, mark: .mercedes, productionYear: 2018, trunkVolume: 10, engineState: .off, windowsState: .close, trunkState: .empty)
+
+//var truck1 = Vehicle(vehicleType: .truck, mark: .volvo, productionYear: 2018, trunkVolume: 100, engineState: .off, windowsState: .close, trunkState: .full)
 
 
     
     
+
+
 
 
 
