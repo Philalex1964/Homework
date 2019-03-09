77c166c936f17754e879cbf0eb3427e23ed28984
Alexander <Philalex196401@gmail.com>
2019-03-09 12:57:32 +0300

    4lhw

---
+++ 4l_FilippovAlexander/4l_FilippovAlexander/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  4l_FilippovAlexander
+//
+//  Created by Александр Филиппов on 09.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
