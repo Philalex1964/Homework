edb931fdbed266b95994aad4ca157b0d82810ae9
Alexander <Philalex196401@gmail.com>
2019-03-11 16:48:14 +0300

    Lesson%

---
+++ LessonFifth/LessonFifth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LessonFifth
+//
+//  Created by Александр Филиппов on 11.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
