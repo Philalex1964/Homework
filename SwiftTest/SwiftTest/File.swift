5ce2039c75999815f171122da8eaf958fe41f814
Alexander <Philalex196401@gmail.com>
2019-03-28 17:10:53 +0300

    TestPassed

---
+++ SwiftTest/SwiftTest/File.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  SwiftTest
+//
+//  Created by Александр Филиппов on 28.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import Foundation
+
+//let x = 3
+//switch x {
+// case 0 : print("0")
+// case 1..<5 : print("1-5")
+// default: print(">5")
+//}
+
+//let point = (-2,2)
+//switch point {
+//case (0,0) :  <#code#>
+//default:
+//    <#code#>
+//}
+
+let text = ["h", "w", "!"]
+print(text[3])
